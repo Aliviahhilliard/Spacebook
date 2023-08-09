58a969750d180129f8bd3240cacf019d6c294fe7
[Elijah <[efrancis64@gmail.com]>
2023-08-09 19:18:17 -0400

    added routes, added comment and post models, updated model index with comments and posts, added login and signup logic for authentication

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: 'area51',
   cookie: {
     maxAge: 300000,
     httpOnly: true,
