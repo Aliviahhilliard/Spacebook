7075bf0575d33fc59c6e16bc7df34277e67ae280
MCKIT77 <jakemckit@gmail.com>
2023-08-16 19:55:46 -0400

    fixed translate buttons and added some style

--- public/js/toggle-font.js
+++ public/js/toggle-font.js
@@ -1,15 +1,30 @@
 const toggleFontButtonHandler = (event) => {
     const contentType = event.target.getAttribute('data-content-type');
-    const contentElements = document.querySelectorAll(`.${contentType}-content`);
 
-    contentElements.forEach((element) => {
-        element.classList.toggle('wingdings-font');
-    });
+    // Check if it's the "Translate All" button
+    if (contentType === 'all') {
+        const contentElements = document.querySelectorAll('.thread-content, .comment-content');
+
+        contentElements.forEach((element) => {
+            element.classList.toggle('font-wingdings');
+        });
+    } else {
+        const contentElements = document.querySelectorAll(`.${contentType}-content`);
+
+        contentElements.forEach((element) => {
+            element.classList.toggle('font-wingdings');
+        });
+    }
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-    const toggleFontButtons = document.querySelectorAll('.toggle-font-button');
-    toggleFontButtons.forEach((button) => {
-        button.addEventListener('click', toggleFontButtonHandler);
+    const toggleFontButton = document.querySelector('.toggle-font-button');
+
+    toggleFontButton.addEventListener('click', () => {
+        const contentElements = document.querySelectorAll('.font-wingdings');
+        contentElements.forEach((element) => {
+            element.classList.toggle('font-default');
+            element.classList.toggle('font-wingdings');
+        });
     });
 });
