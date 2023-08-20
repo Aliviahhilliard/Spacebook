ee21cef2a7c4bfb835e29819cbab3689521c877f
Aliviah Hilliard <aliviahhilliard@gmail.com>
2023-08-20 16:31:55 -0400

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+//const Sequelize = require('sequelize');
+//equire('dotenv').config();
 
 let sequelize;
 
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_PW,
     {
       host: 'localhost',
       dialect: 'mysql',
@@ -18,4 +18,4 @@ if (process.env.JAWSDB_URL) {
   );
 }
 
-module.exports = sequelize;
+//module.exports = sequelize;
